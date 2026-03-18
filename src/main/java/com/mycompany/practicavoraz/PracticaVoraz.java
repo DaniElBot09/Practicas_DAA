3b409dca4d6597eb8bc89bb24fae4b8caf527c70
Diego <diegocaballerosanchez05@gmail.com>
2026-03-18 21:08:44 +0100

    Subiendo mi practica de DAA

---
+++ src/main/java/com/mycompany/practicavoraz/PracticaVoraz.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicavoraz;
+
+/**
+ *
+ * @author diego
+ */
+public class PracticaVoraz {
+
+    public static void main(String[] args) {
+        ;
+    }
+}
