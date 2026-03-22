ffd9f460ae93352bf2d6de20438c892a8e09fa60
DaniElBot09 <royferd9@gmail.com>
2026-03-22 02:29:39 +0100

    Refactor voraz method to use opciones.compatibles

--- src/main/java/com/mycompany/practicavoraz/Algoritmo.java
+++ src/main/java/com/mycompany/practicavoraz/Algoritmo.java
@@ -10,28 +10,21 @@ import java.util.*;
  */
 
 public abstract class Algoritmo {
-    private static boolean compatibles(Actividades opciones, int i, int j){
-        int[] actividad1 = opciones.getActividad(i);
-        int[] actividad2 = opciones.getActividad(j);
-        //Si la actividad 1 termina más tarde que empieza la actividad 2, no son compatibles
-        boolean sol = !(actividad2[1] < actividad1[2]);  
-        return sol;
-    }
     public static ArrayList<Integer> voraz(Actividades opciones){
-       opciones.ordenar();
-       ArrayList <Integer> listaIndex = new ArrayList<>();
-        listaIndex.add(0); 
-        for(int i = 1; i<opciones.length(); i++){
-           if(compatibles(opciones, listaIndex.get(listaIndex.size()-1), i)){
-               listaIndex.add(i);
-           }
-       }
-        ArrayList <Integer> listaIndexEntrada = new ArrayList<>();
-        //Cambiamos por los índices de entrada
-       for(int i = 0; i<listaIndex.size(); i++){
-           int[] actividad = opciones.getActividad((int)listaIndex.get(i));
-           listaIndexEntrada.add(actividad[0]);
-       }
-       return listaIndexEntrada;
-    }    
+    opciones.ordenar();
+    ArrayList <Integer> listaIndex = new ArrayList<>();
+    listaIndex.add(0); 
+    for(int i = 1; i<opciones.size(); i++){
+        if(opciones.compatibles(listaIndex.get(listaIndex.size()-1), i)){
+            listaIndex.add(i);
+        }
+    }
+    ArrayList <Integer> listaIndexEntrada = new ArrayList<>();
+    //Cambiamos por los índices de entrada
+    for(int i = 0; i<listaIndex.size(); i++){
+        int[] actividad = opciones.getActividad((int)listaIndex.get(i));
+        listaIndexEntrada.add(actividad[0]);
+    }
+   return listaIndexEntrada;
+   }    
 }
