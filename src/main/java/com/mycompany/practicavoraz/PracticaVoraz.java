d031affdc1dba5213501a9b9ba6bb34e13e07e25
DaniElBot09 <royferd9@gmail.com>
2026-03-22 02:30:27 +0100

    Refactor main method and add output file handling
    Refactor main method to improve readability and handle output file.

--- src/main/java/com/mycompany/practicavoraz/PracticaVoraz.java
+++ src/main/java/com/mycompany/practicavoraz/PracticaVoraz.java
@@ -10,51 +10,51 @@ import java.io.*;
  * @author diego
  */
 public class PracticaVoraz {
-
-    public static void main(String[] args) {
-       
-ArrayList<int[]> datos = new ArrayList<>();
-    Scanner sc = null;
-
-    try {
-        // Desde un fichero
-        if (args.length > 0) {
-            File fichero = new File(args[0]);
-            sc = new Scanner(fichero);
-        } else {   // Desde el teclado
-            System.out.println("Introduzca el numero de tareas:");
-            sc = new Scanner(System.in);
-        }
-        if (sc.hasNextInt()) {
-            int n = sc.nextInt();
-            for (int i = 0; i < n; i++) {
-                int inicio = sc.nextInt();
-                int fin = sc.nextInt();
-                datos.add(new int[]{inicio, fin});
-            }
-        }
-
-        ArrayList<Integer> sol = Algoritmo.voraz(new Actividades(datos));
-        
-        if (args.length > 1) {
-        File ficheroSalida = new File(args[1]);
     
-        if (ficheroSalida.exists()) {
-        System.err.println("Error: El fichero de salida '" + args[1] + "' ya existe.");
-        return; 
-        }
+    public static void main(String[] args){
+        ArrayList<int[]> datos = new ArrayList<>();
+        Scanner sc = null;
+        boolean porTeclado = !(args.length > 0);
+        try{
+            if(!porTeclado){        // Desde un fichero
+                File fichero = new File(args[0]);
+                sc = new Scanner(fichero);
+            }else{                      // Desde el teclado
+                System.out.println("Introduzca el numero de tareas: ");
+                sc = new Scanner(System.in);
+            }
+            if(sc.hasNextInt()){
+                int n = sc.nextInt();
+                for(int i = 0; i < n; i++){
+                    if(porTeclado) {
+                        System.out.println("Tarea número... " + i); 
+                        System.out.print("Inicio: "); 
+                    }
+                    int inicio = sc.nextInt();
+                    if(porTeclado) System.out.print("Fin: ");
+                    int fin = sc.nextInt();
+                    datos.add(new int[] {inicio, fin});
+                }
+            }
 
-            PrintWriter writer = new PrintWriter(ficheroSalida);
-            writer.println("Tareas seleccionadas: " + sol);
-            writer.close();
-        } else {
-            System.out.println("Resultado: " + sol);
-        }
+            ArrayList<Integer> sol = Algoritmo.voraz(new Actividades(datos));
 
-    } catch (Exception e) {
-        System.err.println("Error: " + e.getMessage());
-    } finally {
-        if (sc != null) sc.close();
+            if(args.length > 1){
+                File ficheroSalida = new File(args[1]);
+                if(ficheroSalida.exists()){
+                    System.err.println("Error: El fichero de salida '" + args[1] + "' ya existe.");
+                    return; 
+                }
+                PrintWriter writer = new PrintWriter(ficheroSalida);
+                writer.println("Tareas seleccionadas: " + sol);
+                writer.close();
+            }else{
+                System.out.println("Selección de tamaño maximal: " + sol);
+            }
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        } finally {
+            if (sc != null) sc.close();
+        }
     }
-}
 }
